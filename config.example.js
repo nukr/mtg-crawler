b2b46a68558b446266f492e32e755bdd3ff0faf7
WuKong <tyler.boright@gmail.com>
2015-10-05 11:10:31 +0800

    changed config example

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 export default {
   credentials: {
-    accessKeyId: 'AKIAI5JKGBMCFUOABZ7Q',
-    secretAccessKey: 'CYWZuu5CqGW1g3lDOZtz7oqY+4146FaYp1HDBuBZ'
+    accessKeyId: 'id',
+    secretAccessKey: 'secret'
   }
 }
